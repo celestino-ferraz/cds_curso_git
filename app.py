be04b03f5fdaded789fa2b6c79a06a9190869158
Fernando_Ferraz <celestino.ferraz.cv@gmail.com>
2023-11-14 16:17:36 -0500

    Criado arquivo de carregamento de dados e ajustado arquivo principal para utilisar esse novo arquivo criado

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from src.extraction import load_data
+
+st.set_page_config(layout='wide')
 
 def load_data():
     return pd.read_csv('C:\Users\celes\repos\curso_git\projeto\data\processed\bikes_completed.csv')
 
 def main():
-    df = load_data()
+    df_raw = load_data()
 
-    st.dataframe(df)
+    st.dataframe(df_raw)
 
 if __name__ == '__main__':
     main()
